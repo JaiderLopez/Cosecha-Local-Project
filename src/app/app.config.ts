bbb8e11353d43eb08748ad260562a8d930cec889
krvinPoli <kevin_florez82202@elpoli.edu.co>
2024-05-24 17:06:01 -0500

    Usuarios registrados se almacenan en firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { getFirestore } from "firebase/firestore";
-import { initializeApp} from "firebase/app";
-// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { initializeApp as initializeApp_alias, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth as getAuth_alias, provideAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCGB8iMhvyHx-ZCUDKLpADq7cBqZp0xBlk",
-  authDomain: "cosecha-local-95669.firebaseapp.com",
-  projectId: "cosecha-local-95669",
-  storageBucket: "cosecha-local-95669.appspot.com",
-  messagingSenderId: "138140106960",
-  appId: "1:138140106960:web:984a65eb3269dfaf0d5ed5",
-  measurementId: "G-62EV3079BX"
-};
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(environment.firebase);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), HttpClientModule,
+  providers: [provideRouter(routes), provideClientHydration(), HttpClientModule, importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"cosechadb","appId":"1:555429064872:web:10a2ef3f46fc90b71c529d","storageBucket":"cosechadb.appspot.com","apiKey":"AIzaSyB3rHKKAorA0oWgdpUpHqTG1Yh9_Chj2oQ","authDomain":"cosechadb.firebaseapp.com","messagingSenderId":"555429064872","measurementId":"G-2YX8BP8C6D"}))), importProvidersFrom(provideAuth(() => getAuth())),
     
   ]
   
--- src/app/components/user/register/register.component.ts
+++ src/app/components/user/register/register.component.ts
@@ -2,17 +2,34 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, RouterLink,} from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { Router } from '@angular/router';
+import { UserService } from '../../../services/user.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, LoginComponent,],
+  imports: [RouterLink, LoginComponent, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  applyForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  constructor(private route: ActivatedRoute, private router: Router, private userService: UserService ) {}
+
   goToItems() {
     this.router.navigate(['/login'], { relativeTo: this.route });
+  }
+  onSubmit() {
+    this.userService.toRegister(this.applyForm.value)
+      .then(response => {
+        this.router.navigate(['/home/buy'], { relativeTo: this.route });
+      })
+      .catch(error => console.log(error));
   }
 }
